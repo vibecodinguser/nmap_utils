5eebf3a3491d69904aff105dd9c98de1563b5fab
antonmarin <vibecodinguser@gmail.com>
2025-11-13 20:03:24 +0300

    добавлены зависимости

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 """Конфигурация и константы приложения"""
 
-YANDEX_DISK_API_KEY = "_your_OAuth_token_here_"
+YANDEX_DISK_API_KEY = "y0__xCG04IkGNuWAyDby7THExzFQIJhChaEP4AuVz5pqCI82CE3"
 BASE_FOLDER_PATH = "Приложения/Блокнот картографа Народной карты"
