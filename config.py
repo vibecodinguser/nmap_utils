09a6727cd1fc691d46ec352f0b018d9f27c07ada
Anton Marin <vibecodinguser@gmail.com>
2025-11-18 15:05:22 +0300

    обновил конфиг

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 """Конфигурация и константы приложения"""
 
-YANDEX_DISK_API_KEY = "y0__xCG04IkGNuWAyDby7THExzFQIJhChaEP4AuVz5pqCI82CE3"
+YANDEX_DISK_API_KEY = "_your_OAuth_token_here_"
 BASE_FOLDER_PATH = "Приложения/Блокнот картографа Народной карты"
