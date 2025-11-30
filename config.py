74d00ce6fc9e44aeadb80d1addb3f32e312d8a54
antonmarin <vibecodinguser@gmail.com>
2025-11-30 21:02:15 +0300

    добавлен конфиг

--- config.py
+++
@@ -1,9 +0,0 @@
-"""
-Получение OAuth-токена
-
-Для загрузки данных в Блокнот картографа на Яндекс Диск потребуется получить OAuth-токен
-тут https://yandex.ru/dev/disk/poligon/ и вставить вместо YOUR_YANDEX_DISK_API_KEY
-"""
-
-YANDEX_DISK_API_KEY = "YOUR_YANDEX_DISK_API_KEY"
-
